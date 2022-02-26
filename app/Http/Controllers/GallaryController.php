fa1cf4c7660654bbd8338aeffe032e7b83057200
AhmadAlmokdad <alahmadsy1234@gmail.com>
2022-02-26 07:04:44 +0200

    add4 gallary

---
+++ app/Http/Controllers/GallaryController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Gallary;
+use Illuminate\Http\Request;
+
+class GallaryController extends Controller
+{
+    public function index()
+    {
+        $images = Gallary::all();
+        return view('gallary', compact('images'));
+    }
+
+    public function create()
+    {
+        return view('admin.gallary.create');
+    }
+
+    public function store(Request $request)
+    {
+        $gallary = new Gallary();
+        $image = $request->file('image');
+        $destinationPathImg = public_path('upload/gallary/');
+        if (!$image->move($destinationPathImg, $image->getClientOriginalName())) {
+            return 'Error saving the file.';
+        }
+        $gallary->url = $image->getClientOriginalName();
+        $gallary->save();
+    }
+
+    public function delete($id)
+    {
+        $image = Gallary::find($id)->delete();
+    }
+}
